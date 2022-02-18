4c5c152b656eef04c5bc992e8f2d2f27a21b8e9e
Jotappe <jotappe-@hotmail.com>
2022-02-18 23:16:36 +0000

    component dashboard

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
 
     --background: #F0F2F5;
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
     --blue-light: #6933FF;
     --text-title: #363F5F;
