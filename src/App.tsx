aedf5ec11b86c9e81c4c503e11348aa5898dec91
Jotappe <jotappe-@hotmail.com>
2022-02-25 10:38:18 +0000

    Modal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
